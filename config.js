96dddf7a1868b3ad1b608947af9f8c324488bb69
Diegoson <130500796+Fenandes@users.noreply.github.com>
2023-04-12 16:36:16 +0200

    ♻

---
+++ config.js
@@ -0,0 +1,42 @@
+//-------------------------------------------------------------------------------*/
+
+
+require("dotenv").config();
+let gg = process.env.MODS;
+if (!gg) {
+  gg = "27686881509";   
+}
+
+// -------------------------------------------------------------- //
+
+
+global.owner = gg.split(",");
+global.mongodb = process.env.MONGODB || "mongodb+srv://fantox:xrto71r@cluster0.dnlowts.mongodb.net/?retryWrites=true&w=majority";
+global.sessionId = process.env.SESSION_ID || "ok";
+global.prefa = process.env.PREFIX || "-";
+global.tenorApiKey =
+  process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
+global.packname = process.env.PACKNAME || `Amarok MD`,
+global.port = process.env.PORT || "8000";
+
+module.exports = {
+  mongodb: global.mongodb,
+};
+
+global.mess = {
+  jobdone: "Job done...",
+  useradmin: "Sorry, only *Group Admins* can use this command *Baka*!",
+  botadmin:
+    "Sorry, i cant execute this command without being an *Admin* of this group.",
+  botowner: "Only my *Owner* can use this command, Baka!",
+  grouponly: "This command is only made for *Groups*, Baka!",
+  privateonly: "This command is only made for *Private Chat*, Baka!",
+  botonly: "Only the *Bot itself* can use this command!",
+  waiting: "Chotto Matte...",
+  nolink: "Please provide me *link*, Baka!",
+  error: "An error occurd!",
+  banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* or *${prefa}support* to submit a request to unban yourself !`,
+  bangc: "This Group is *Banned* from using Commands!",
+  nonsfw: "Dont be a pervert Baka! This is not a NSFW enabled group!",
+};
+
