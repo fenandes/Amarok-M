af8fa693d988d49e057fd566aa5f3c9ebaf6a68f
Diegoson <130500796+Fenandes@users.noreply.github.com>
2023-04-13 17:29:56 +0200

    Create _dare.js

---
+++ plugins/_dare.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+module.exports = {
+  name: "dare",
+  alias: ["givedare", "d"],
+  desc: "give a dare",
+  cool: 3,
+  react: "😐",
+  category: "Fun",
+  start: async (Amarok, m, { text, prefix }) => {
+    const shibam = await axios.get(
+      "https://dull-plum-panda-gear.cyclic.app/dare"
+    );
+    
+    await Amarok.sendMessage(m.from, {image: { url: botImage4 },caption: `*${shibam.data}*`,}, { quoted: m });
+  },
+};
