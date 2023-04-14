190e0d256804e7120073629dd452d2c615f74f67
Diegoson <130500796+Fenandes@users.noreply.github.com>
2023-04-14 18:23:24 +0200

    Create gjid.js

---
+++ plugins/Jid/gjid.js
@@ -0,0 +1,11 @@
+module.exports = {
+name: "jid",
+alias: ["gjid"],
+desc: "check number jid",
+react: "🎈",
+category: "Group",
+start: 
+       async(Amarok,m {text,prefix}) => {
+
+                               await Amarok.reply(m.from, jid
+  } ));
