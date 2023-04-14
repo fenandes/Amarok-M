ca358390335800175f841cd0e3f41329ff4f06d0
Diegoson <130500796+Fenandes@users.noreply.github.com>
2023-04-14 18:14:57 +0200

    Create runtime.js

---
+++ plugins/Speed/runtime.js
@@ -0,0 +1,11 @@
+ module.exports = {
+ name: "runtime",
+ alias: ["runtime,process"],
+ desc: "check the bot runtime",
+ react: "⌚",
+ category: "Speed",
+ start: async(Amarok,m, {text,prefix}) => {
+
+ await Amarok.reply(runtime(process.uptime())
+ }
+);
