7271dd6931d81f5e3cf62c5e981a63b2e24e616a
Diegoson <130500796+Fenandes@users.noreply.github.com>
2023-04-19 05:47:27 +0200

    Create _withdraw.js

---
+++ plugins/Games/_withdraw.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const config = require("../../config");
+const eco = require("discord-mongoose-economy");
+const ty = eco.connect(config.mongodb);
+
+module.exports = {
+  name: "withdraw",
+  desc: "withdraw money from bank account.",
+  alias: ["withdraw"],
+  category: "Economy",
+  react: "💳",
+  start: async (
+    Amarok,
+    m,
+    { text, prefix, isBotAdmin, isAdmin, mentionByTag, pushName, isCreator }
+  ) => {
+    if (!text) {
+      return Amarok.sendMessage(
+        m.from,
+        { text: `*Provide the amount you want to withdraw!*` },
+        { quoted: m }
+      );
+    }
+    const user = m.sender;
+    const query = text.trim();
+    const cara = "cara";
+    const withdraw = await eco.withdraw(user, cara, query);
+    if (withdraw.noten)
+      Amarok.sendMessage(
+        m.from,
+        { text: "*🏧 Insufficient fund in bank*" },
+        { quoted: m }
+      );
+    const add = eco.give(user, cara, query);
+    Amarok.sendMessage(
+      m.from,
+      {
+        image: fs.readFileSync("./Assets/Img/card.png"),
+        caption: `*🏧 ALERT*  _💶 ${withdraw.amount} has been added in your wallet._*`,
+      },
+      { quoted: m }
+    );
+  },
+};
