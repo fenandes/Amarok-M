d9b52622c348852702e4fe0c6954933bd38f534c
Diegoson <130500796+Fenandes@users.noreply.github.com>
2023-04-22 14:27:03 +0200

    Update _menu.js

--- plugins/Menu/_menu.js
+++ plugins/Menu/_menu.js
@@ -178,7 +178,7 @@ const now = new Date();
 
       textHelpMenu += `*🔰  ${botName}  🔰*
  _Powered By:_ *Team ATLAS*
-      await Amarok.sendMessage(m.from, {image: fs.readFileSync("./lib/amarok/amarok.png"), caption: textHelpMenu,}, { quoted: m });
+      await Amarok.sendMessage(m.from, {image: fs.readFileSync("./lib/amarok/amarok.jpg"), caption: textHelpMenu,}, { quoted: m });
     }
   },
 };
