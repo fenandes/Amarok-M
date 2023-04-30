3fc22cd244abb922268b8cf786a7b9135e608035
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-04-30 08:09:39 +0200

    Update _slot.js

--- plugins/Games/_slot.js
+++ plugins/Games/_slot.js
@@ -1,4 +1,4 @@
-const config = require('../../lib/config');
+const config = require('../../lib/config.js');
 const eco = require('discord-mongoose-economy')
 const ty = eco.connect(config.mongodb);
 
