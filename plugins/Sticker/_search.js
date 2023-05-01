a3bf6bf20b43be26f466caa3fce3df47078d3550
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-05-01 13:29:20 +0200

    Create _search.js

---
+++ plugins/Sticker/_search.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const { Sticker, StickerTypes } = require("wa-sticker-formatter");
+
+module.exports = {
+  name: "stickersearch",
+  alias: ["getsticker", "searchsticker"],
+  desc: "To search any sticker",
+  category: "Converter",
+  usage: `stickersearch <search term>`,
+  react: "♻",
+  start: async (Amarok, m, { text, prefix, args, pushName }) => {
+    if (!args[0])
+      return Amarok.sendMessage(
+        m.from,
+        { text: `Please provide a Search Term !` },
+        { quoted: m }
+      );
+    var gifSearchTerm = args.join(" ");
+    const gif = await axios.get(
+      `https://tenor.googleapis.com/v2/search?q=${gifSearchTerm}&key=${tenorApiKey}&client_key=my_project&limit=8&media_filter=gif`
+    );
+
+    let result = Math.floor(Math.random() * 8);
+    let gifUrl = gif.data.results[result].media_formats.gif.url;
+
+    const response = await axios.get(gifUrl, {
+      responseType: "arraybuffer",
+    });
+    const buffer = Buffer.from(response.data, "utf-8");
+
+    let stickerMess = new Sticker(buffer, {
+      pack: packname,
+      author: pushName,
+      type: StickerTypes.FULL,
+      categories: ["🤩", "🎉"],
+      id: "12345",
+      quality: 60,
+      background: "transparent",
+    });
+    const stickerBuffer2 = await stickerMess.toBuffer();
+    Amarok.sendMessage(m.from, { sticker: stickerBuffer2 }, { quoted: m });
+  },
+};
