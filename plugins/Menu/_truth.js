36b3a8ddc667fa547dbd8dfbfb8ad1811297c211
Diegoson <130500796+fenandes@users.noreply.github.com>
2023-05-03 19:29:35 +0200

    Create _truth.js

---
+++ plugins/Menu/_truth.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+module.exports = {
+  name: "truth",
+  alias: ["t"],
+  desc: "truth",
+  cool: 3,
+  react: "😀",
+  category: "Fun",
+  start: async (Amarok, m, { text, prefix }) => {
+    const shibam = await axios.get(
+      "https://dull-plum-panda-gear.cyclic.app/truth"
+    );
+
+    await Amarok.sendMessage(m.from, {image: { url: botImage3 },caption: `*${shibam.data}*`,}, { quoted: m });
+  },
+};
